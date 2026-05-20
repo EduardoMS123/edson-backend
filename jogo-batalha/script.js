b5aabfd8a24369f4e97f3e508d191750b0a3127d
EduardoMS <eduardo.montanha.santos@escola.pr.gov.br>
2026-05-20 11:52:45 -0300

    Add files via upload

--- jogo-batalha/script.js
+++ jogo-batalha/script.js
@@ -11,6 +11,39 @@ function lutar(){
     const dado = Math.floor(Math.random()*20) + 1;
     let resultado = `🎲 Dado: ${dado} | <strong>${classe}<strong> com ${arma}`
 
+    switch(classe){
+        case "Guerreiro": 
+        if(arma.toLowerCase() === "espada" && dado > 5){
+            resultado += " <span class = 'sucesso'> SUCESSO: Você matou o dragão com uma espada!</span>"
+            log.innerHTML = resultado
+        
+        }else if(dado > 15){
+            resultado += "<span class = 'sucesso'> VITÓRIA: você MERENDOU ele na porrada, boaaa!</span>"
+            log.innerHTML = resultado
+
+        }else{
+            resultado += "<span class = 'derrota'> DERROTA: O dragão te moer na porrada seu lixo!!</span>"
+            log.innerHTML = resultado
+        
+        };
+        case "Mago": 
+        if(arma.toLowerCase() === "cajado" && dado > 5){
+            resultado += "<span class = 'sucesso'> VITÓRIA: você ESTOUROU o dragão com uma magia básica, boaaa!</span>"
+            log.innerHTML = resultado
+        
+        }else if(dado > 15){
+            resultado += "<span class = 'sucesso'> SUCESSO: Você matou o dragão com uma macumba das locas!</span>"
+            log.innerHTML = resultado
+
+        }else{
+            resultado += "<span class = 'derrota'> DERROTA: O CARA PERDEU DE MAGO?????? Tu é ruim ein</span>"
+            log.innerHTML = resultado
+        
+        };
+
+    }
+
+
 }
 
 
