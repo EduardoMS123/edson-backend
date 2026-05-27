c78883076cb9169b997bb5cae4b1798f8b2a0ca8
EduardoMS <eduardo.montanha.santos@escola.pr.gov.br>
2026-05-27 09:50:39 -0300

    Add files via upload

--- 01/Classe_e_objeto.js
+++ 01/Classe_e_objeto.js
@@ -1,10 +1,20 @@
- let pessoa = {
-    nome: "Jão"
+class heroi{
+   constructor(nome, classe){
+      this.nome = nome;
+      this.classe = classe;
+   };
+};
 
- }
+function criarHeroi(){
+//capturando oqueo usuario digitou/selecionou na tela
+   const nomeDigitado = document.getElementById('nomeInput').value;
+   const classeSelecionada = document.getElementById('classeSelect').value;
+//crianddo o objeo dinamicamente com os valores dos campos
+
+   const novoHeroi = new heroi(nomeDigitado, classeSelecionada);
 
 
-
+};
 
 
 
